876e3bc9d336c8aee5b1ba0b77b4f0a2608a53ac
skandesh-kk <skandesh04in@gmail.com>
2025-07-23 23:32:07 +0530

    fully functional with publish page added along with skeletons for loading

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useBlog } from "../hooks"
 import { FullBlog } from "../components/FullBlog";
+import { Appbar } from "../components/AppBar";
+import { BlogSkeleton } from "../components/BlogSkeleton";
 export const Blog = () =>{ 
     const {id}=useParams();
     const {loading,blog}= useBlog({
@@ -8,7 +10,18 @@ export const Blog = () =>{
     });
     console.log("loaded blog is ",blog)
     if(loading){
-        return <div>Loading...</div>
+        return <div>
+            <Appbar /> 
+            <div  className="flex justify-center">
+                <div>
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                </div>
+            </div>
+        </div>
     }
 
     return <div>
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,13 +1,26 @@
 import { BlogCard } from "../components/BlogCard"
 import { Appbar } from "../components/AppBar"
 import { useBlogs } from "../hooks"
+import { BlogSkeleton } from "../components/BlogSkeleton"
 export const Blogs =()=>{
 
     const {loading,blogs}=useBlogs();
     if(loading){
         return <div>
-            loading....
+            <Appbar /> 
+            <div  className="flex justify-center">
+                <div>
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                </div>
+            </div>
         </div>
+    }
+    if (!blogs) {
+    return <div>Blog not found</div>;
     }
 
     return <div>
@@ -15,17 +28,18 @@ export const Blogs =()=>{
         <div className="flex justify-center">
         
         <div className=" ">
-            {blogs.map(blog =>{
-                return(
+            {blogs.map(blog =>(
+                
                 <BlogCard
+                key={blog.id}
                 id={blog.id}
                 authorName={blog.author?.name || "Anonymous"}
                 title={blog.title}
                 content={blog.content}
                 publishedDate={"2nd Feb 2024"}
             />
-            );
-            })}
+            
+            ))}
             
             
         </div>
